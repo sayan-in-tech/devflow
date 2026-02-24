f445d6c6fec852c731e2ebe2cfdb0dd4a82e7ba3
sayan <sayan18012004@gmail.com>
2026-02-24 13:55:14 +0530

    git checks trial

--- tests/integration/cli_tests.rs
+++ tests/integration/cli_tests.rs
@@ -1,10 +1,10 @@
-use assert_cmd::Command;
+use assert_cmd::{cargo::cargo_bin_cmd, prelude::*};
 use predicates::str::contains;
 
 #[test]
 fn init_creates_config() {
     let td = tempfile::tempdir().expect("tempdir");
-    Command::cargo_bin("devflow")
+    cargo_bin_cmd!("devflow")
         .expect("binary")
         .current_dir(td.path())
         .arg("init")
@@ -18,7 +18,7 @@ fn init_creates_config() {
 #[test]
 fn port_free_outputs_json_list() {
     let td = tempfile::tempdir().expect("tempdir");
-    Command::cargo_bin("devflow")
+    cargo_bin_cmd!("devflow")
         .expect("binary")
         .current_dir(td.path())
         .args(["port", "--free"])
